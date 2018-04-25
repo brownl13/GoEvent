02ae8547552fa30ac95f4b994b2d7eeb74178ed3
Logan <LRB@JBLaptop.local>
2018-04-25 13:26:03 -0400

    back button working

--- GoEvent/loadingTabBarController.swift
+++ GoEvent/loadingTabBarController.swift
@@ -10,5 +10,10 @@ import Foundation
 import UIKit
 
 class loadingTabBarController: UITabBarController {
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+        
+        // Do any additional setup after loading the view.
+    }
 }
